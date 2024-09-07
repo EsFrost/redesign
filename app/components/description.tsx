d3f99da7ae2e7c2ff60c1b029f2a32c5d33ef899
Sigmund Frost <sigmafi.tech@gmail.com>
2024-09-07 15:48:58 +0200

    Finished description section

--- app/components/description.tsx
+++ app/components/description.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Heart } from "lucide-react";
+import Image from "next/image";
 
 export const Description = () => {
   const services = [
@@ -53,8 +54,8 @@ export const Description = () => {
     },
   ];
   return (
-    <section className="text-center bg-[#F6F6F6]">
-      <div className="flex justify-center items-center gap-4">
+    <section className="text-center bg-[#F6F6F6] py-12">
+      <div className="flex justify-center items-center gap-4 px-4 lg:px-0">
         <div className="border border-[#e9a257] h-0 w-12"></div>
         <h3 className="text-3xl text-[#e9a257]">
           Video & Photo Production Experts
@@ -62,7 +63,7 @@ export const Description = () => {
         <div className="border border-[#e9a257] h-0 w-12"></div>
       </div>
 
-      <h4 className="text-xl text-gray-600 italic">
+      <h4 className="text-xl text-gray-600 italic mt-2">
         Καλύπτουμε όλες τις ανάγκες σας για βίντεο & φωτογραφία
       </h4>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-8">
@@ -86,6 +87,38 @@ export const Description = () => {
             </ul>
           </div>
         ))}
+      </div>
+      <div className="mt-12">
+        <div className="flex justify-center items-center gap-4 px-4 lg:px-0">
+          <div className="border border-[#e9a257] h-0 w-12"></div>
+          <h3 className="text-3xl text-[#e9a257]">
+            Ψηφιακά άλμπουμ & εκτυπώσεις
+          </h3>
+          <div className="border border-[#e9a257] h-0 w-12"></div>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mx-auto max-w-7xl mt-4">
+          <Image
+            width={1000}
+            height={1000}
+            src={"/photo-album.jpg"}
+            alt="Album of photos"
+            className="w-full h-auto px-4 md:w-1/3"
+          />
+          <div className="flex flex-col text-gray-600 px-4 lg:px-0 text-sm max-w-xl mt-8 md:mt-0">
+            <p>
+              Επιλέξτε να εκτυπώσετε σε εμάς τα ψηφιακά σας άλμπουμ ή και τις
+              φωτογραφίες απο τον γάμο, την βάπτιση ή την εκδήλωση σας και
+              κερδίστε μοναδικές εκπτώσεις!
+            </p>
+            <p className="mt-4">
+              Αναλαμβάνουμε εκτύπωση σε κάθε είδους υλικό όπως καμβά, κούπα,
+              πέτρα, φωτογραφικό και μή χαρτί, καθώς και οτιδήποτε άλλο θέλετε
+            </p>
+            <p className="mt-4">
+              Επικοινωνήστε μαζί μας για να μάθετε τιμές και προσφορές!
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
